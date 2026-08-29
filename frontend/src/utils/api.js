da13b33a350d4b5f11908c7ea2dc4b0ad75e4d51
Karthick R <john8012karthik@gmail.com>
2026-08-29 22:19:37 +0530

    feat: update API_URL configuration for development and production environments

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.MODE ==="development" ? "http://localhost:5000/api" :"/api";
 
 if (!API_URL) {
   throw new Error(
